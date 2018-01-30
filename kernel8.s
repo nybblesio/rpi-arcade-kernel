ebe89f31f569fda49c92d218cdc4968ff467fed7
jeffpanici75 <jeff.panici@panici-software.com>
2018-01-29 18:55:03 -0700

    work on rendering foreground tile

--- kernel8.s
+++ kernel8.s
@@ -109,25 +109,25 @@ frame_buffer_commands:
         palette                 bus_cmd Set_Palette,          264,   0,            64
         palette_data:        
                 ; palette 1
-                dw $2492ffff, $ff0000ff, $b60000ff, $ff0049ff 
+                dw $2492ff00, $ff0000ff, $b60000ff, $ff0049ff 
                 dw $db9224ff, $00006dff, $6d6d49ff, $494924ff 
                 dw $00006dff, $000000ff, $db6d24ff, $6d2400ff 
                 dw $924900ff, $004900ff, $006d00ff, $ffffffff    
 
                 ; palette 2
-                dw $6d4900ff, $922400ff, $db9200ff, $492400ff
+                dw $6d490000, $922400ff, $db9200ff, $492400ff
                 dw $b66d00ff, $6d2400ff, $006d00ff, $0024b6ff 
                 dw $ffffffff, $000000ff, $2492ffff, $ff0000ff 
                 dw $6d6d6dff, $494949ff, $00006dff, $ffffffff
 
                 ; palette 3
-                dw $000000ff, $0092ffff, $00006dff, $ff0049ff
+                dw $00000000, $0092ffff, $00006dff, $ff0049ff
                 dw $922400ff, $494949ff, $6d6d49ff, $494924ff
                 dw $ffffffff, $000000ff, $db6d24ff, $6d2400ff
                 dw $924900ff, $004900ff, $006d00ff, $ffffffff
 
                 ; palette 4
-                dw $2492ffff, $ff0000ff, $db9200ff, $ff0049ff
+                dw $2492ff00, $ff0000ff, $db9200ff, $ff0049ff
                 dw $b64900ff, $6d2400ff, $6d6d49ff, $494949ff
                 dw $b60000ff, $000000ff, $db6d24ff, $6d2400ff
                 dw $924900ff, $004900ff, $006d00ff, $ffffffff
@@ -147,7 +147,19 @@ align 8
 sys_font        font            8, 8, sys_font_ptr
 
 align 8
-sys_font_ptr:   include 'font8x8.s'
+sys_font_ptr:   include         'font8x8.s'
+
+align 8
+timber_bg0:     file            'assets/timbg0.bin'
+timber_bg1:     file            'assets/timbg1.bin'
+timber_fg0:     file            'assets/timfg0.bin'
+timber_fg1:     file            'assets/timfg1.bin'
+timber_fg2:     file            'assets/timfg2.bin'
+timber_fg3:     file            'assets/timfg3.bin'
+timber_fg4:     file            'assets/timfg4.bin'
+timber_fg5:     file            'assets/timfg5.bin'
+timber_fg6:     file            'assets/timfg6.bin'
+timber_fg7:     file            'assets/timfg7.bin'
 
 ; -------------------------------------------------------------------------
 ;
@@ -192,7 +204,7 @@ frame_buffer_init:
         add         w1, w1, w3
         add         w0, w0, w1
 
-        adr         x1, sys_font.ptr
+        adr         x1, sys_font.ptr + 8
         adr         x2, title        
         mov         w3, title.size
         ldr         w10, [sys_font.w_stride]
@@ -211,6 +223,37 @@ draw_char:
         sub         x0, x0, x11
         subs        w3, w3, 1
         b.ne        draw_string
+
+; ------------------------------
+;
+;
+; ------------------------------
+        adr         x1, frame_buffer.data1
+        ldr         w0, [x1]
+        mov         w1, SCREEN_WIDTH
+        mov         w2, 32
+        mov         w3, 32
+        mul         w1, w1, w2
+        add         w1, w1, w3
+        add         w0, w0, w1
+
+        adr         x1, timber_fg0
+        mov         x5, 512
+        mov         w3, 32        
+draw_raster:        
+        mov         w4, 16
+draw_pixels:
+        ldrb        x6, [x5], 1
+        lsr         x7, x6, 4
+        and         x8, x6, $0f
+        strb        x7, [x0], 1
+        strb        x8, [x0, 1]
+        add         x0, x0, 2
+        subs        w4, w4, 1
+        b.ne        draw_pixels
+        add         x0, x0, SCREEN_WIDTH - 32
+        subs        w3, w3, 1
+        b.ne        draw_raster
 
 main_core_busy_loop:
         b           main_core_busy_loop
